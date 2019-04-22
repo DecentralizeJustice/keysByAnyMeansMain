b53d064e51c78f2902f6bc44ebca9c6f3c54c453
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-04-21 22:36:31 -0700

    Clean Slate

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-app dark>
+    <v-app>
       <v-content>
         <router-view/>
     </v-content>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,12 @@
 <template>
   <v-container  text-xs-center fluid ma-0 pa-0 >
-    <v-layout row wrap ma-0 pa-0>
-      <v-flex xs12 ma-0 pa-0 hidden-md-and-down style="overflow: hidden;">
-        <video style="
-        position: fixed;
-        right: 0;
-        bottom: 0;
-        min-width: 100%;
-        min-height: 100%;"
-        src="https://res.cloudinary.com/dylevfpbl/video/upload/v1551009310/decenMain/yes.mp4" autoplay muted loop/>
+    <v-layout row wrap ma-0 pa-0 style="">
 
-        <v-flex class="title" md8 offset-xs2 pa-2 >
-          <div class="display-4 white--text text-xs-center" >
-            Decentralize Justice
-          </div>
-          <br>
-          <div class="text-xs-center">
-            <v-btn flat class="buttonOpt" to="/projects">Projects</v-btn>
-            <v-btn flat class="buttonOpt" >Shop</v-btn>
-            <v-btn flat class="buttonOpt" >About</v-btn>
-            <v-btn flat class="buttonOpt" href="https://www.decentralizejustice.com/blog">Blog</v-btn>
-          </div>
-        </v-flex>
-      </v-flex>
-
-      <v-flex xs12 ma-0 pa-0 hidden-md-and-up  style="background-image: url('https://images.pexels.com/photos/912110/pexels-photo-912110.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');height:100vh;width:100vw;  background-repeat: no-repeat;
-  background-size: cover; ">
-        <v-flex class="title" xs12 pa-4 style="background-color: #64B5F6;">
-          <div class="display-2 white--text text-xs-center" >
-            Decentralize Justice
-          </div>
-          <br>
-          <div class="text-xs-center">
-            <v-btn flat class="buttonOpt" to="/projects">Projects</v-btn>
-            <v-btn flat class="buttonOpt" >Shop</v-btn>
-            <v-btn flat class="buttonOpt" >About</v-btn>
-            <v-btn flat class="buttonOpt" href="https://www.decentralizejustice.com/blog">Blog</v-btn>
-          </div>
-        </v-flex>
-      </v-flex>
       </v-layout>
   </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'home',
   components: {
